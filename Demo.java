e5efb141824fdc9f6958a06672844dc2f82a82fb
239x1a2953-dot <239x1a2953@gprec.ac.in>
2025-09-02 16:27:23 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println(10);
+        System.out.println(10+20);
+        System.out.println(10+20+"value is");
+        System.out.println(10+20+"value is"+10);
+        System.out.println(10+20+"value is"+10+20);
+        System.out.println(10+20+"value is"+(10+20));
+
+	}
+
+}
