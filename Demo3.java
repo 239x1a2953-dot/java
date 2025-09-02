d7244814ce2ad2495b2ac37284b54802bea2444b
239x1a2953-dot <239x1a2953@gprec.ac.in>
2025-09-02 17:37:21 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,16 @@
+package day2;
+
+class Demo3 {
+static void Display() {
+	System.out.println("java");
+	System.out.println("SQL");
+	System.out.println("Python");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Display();
+Display();
+	}
+	
+
+}
