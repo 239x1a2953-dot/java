1e04daa029b4bc707d0eba00c63886574ffb2ade
239x1a2953-dot <239x1a2953@gprec.ac.in>
2025-09-02 16:26:11 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=99;
+double d=125.78;
+System.out.println("i="+i);
+System.out.println("d="+d);
+System.out.println("After primitive type conversion");
+d=i;
+System.out.println("i"+i);
+System.out.println("d="+d);
+	}
+
+}
