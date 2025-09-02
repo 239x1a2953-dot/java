47137fd27320604303ed265e4f7e44aed6e82bc0
239x1a2953-dot <239x1a2953@gprec.ac.in>
2025-09-02 17:14:39 +0530

    Add files via upload

---
+++ Datanarrowing.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class Datanarrowing {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double d=125.78;
+int i=99;
+System.out.println("d="+d);
+System.out.println("i="+i);
+System.out.println("After type conversion");
+i=(int)d;
+System.out.println("d="+d);
+System.out.println("i="+i);
+	}
+
+}
+
---
+++ Example1.java
@@ -0,0 +1,15 @@
+package day2;
+
+class Example1 {
+	static void Helloworld() {
+		System.out.println("Helloworld");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Helloworld();
+Helloworld();
+Helloworld();
+	}
+
+}
